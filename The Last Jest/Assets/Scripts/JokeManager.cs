f33509210d31018c71e12260a4bbe54d64cb9c4f
Domenico Favaro <daff0111.df@gmail.com>
2024-01-27 13:48:00 +0100

    Added Restart option and Jokes Count

--- The Last Jest/Assets/Scripts/JokeManager.cs
+++ The Last Jest/Assets/Scripts/JokeManager.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 using UnityEngine;
 
 public class JokeManager : MonoBehaviour
@@ -18,15 +19,30 @@ public class JokeManager : MonoBehaviour
     public Character Princess;
     public ExecutionerCharacter Executioner;
 
+    protected int JokeCount = 0;
+    protected bool GameOver = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        JokeCount = 0;
+        GameOver = false;
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (GameOver)
+        {
+            if (Input.GetKeyDown(KeyCode.R))
+            {
+                // Reload Game
+                string currentSceneName = SceneManager.GetActiveScene().name;
+                SceneManager.LoadScene(currentSceneName);
+            }
+            return;
+        }
+
         if (Input.GetKeyDown(KeyCode.A))
         {
             TellJoke(EJokeType.A);
@@ -50,27 +66,31 @@ public class JokeManager : MonoBehaviour
 
     void TellJoke(EJokeType joke)
     {
+        JokeCount++;
         Debug.Log("Joke "+ joke.ToString() + " Told");
         switch (joke) 
         {
             case EJokeType.A:
-                
+                Debug.Log("The Queen likes to slap the Executioner");
                 King.AddAngryMeter(20);
                 Queen.AddAngryMeter(-20);
                 Executioner.AddAngryMeter(-20);
                 break;
             case EJokeType.B:
+                Debug.Log("The King has a big butt");
                 King.AddAngryMeter(-40);
                 Queen.AddAngryMeter(20);
                 Princess.AddAngryMeter(20);
                 break;
             case EJokeType.C:
-                King.AddAngryMeter(-30);
+                Debug.Log("The Princess calls the king Poopface");
+                King.AddAngryMeter(-10);
                 Queen.AddAngryMeter(10);
                 Princess.AddAngryMeter(-20);
                 Executioner.AddAngryMeter(20);
                 break;
             case EJokeType.D:
+                Debug.Log("The Princess is a spoiled Bratt");
                 King.AddAngryMeter(20);
                 Queen.AddAngryMeter(10);
                 Princess.AddAngryMeter(-30);
@@ -89,19 +109,27 @@ public class JokeManager : MonoBehaviour
         if (King.GetAngryMeter() <= 0) 
         {
             // You Died - King Orders your death
-            Executioner.KillJester();
+            EndGame();
+
         }
 
         if (Executioner.GetAngryMeter() <= 0)
         {
             // You Died - The Executioner is angry and he kills you
-            Executioner.KillJester();
             Executioner.RevealAngryFace();
+            EndGame();
         }
 
         if(Queen.GetAngryMeter() <= 0)
         {
             // Queen is Angry - nex day she'll be in the executioner place
         }
+    }
+
+    void EndGame()
+    {
+        Executioner.KillJester();
+        GameOver = true;
+        Debug.Log("YOU DIED - You told " + JokeCount + " Jokes. Press 'R' to Restart");
     }
 }
